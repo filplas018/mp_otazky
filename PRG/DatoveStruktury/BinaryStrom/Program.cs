73c9b50056b0751273506fec6d3f1001f500b7a6
Filip Plass <u18.422@pslib.cloud>
2022-06-07 12:37:09 +0200

    PRG a WEB

---
+++ PRG/DatoveStruktury/BinaryStrom/Program.cs
@@ -0,0 +1,285 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BinaryStrom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Binarni strom!");
+            BinStrom<int> muj_strom = new BinStrom<int>();
+            muj_strom.Add(25);
+            muj_strom.Add(3);
+            muj_strom.Add(10);
+            muj_strom.Add(7);
+            muj_strom.Add(4);
+            muj_strom.Add(5);
+            muj_strom.Add(1);
+            muj_strom.VypisObsah();
+            Console.WriteLine("Vyska: " + muj_strom.Vyska());
+            muj_strom.Print();
+            muj_strom.SmazatUzel(3);
+            muj_strom.VypisObsah();
+            Console.WriteLine("Vyska: " + muj_strom.Vyska());
+            muj_strom.Print();
+        }
+    }
+
+    class BinStrom<T> where T : IComparable
+    {
+        Node koren;
+        class Node
+        {
+            public T hodn;
+            public Node levy, pravy;
+            public Node(T vstup)
+            {
+                hodn = vstup;
+            }
+        }
+
+        class NodeInfo
+        {
+            public Node Node;
+            public string Text;
+            public int StartPos;
+            public int Size { get { return Text.Length; } }
+            public int EndPos { get { return StartPos + Size; } set { StartPos = value - Size; } }
+            public NodeInfo Parent, Left, Right;
+        }
+
+        public void Print()
+        {
+            Print(koren);
+        }
+
+        public int Vyska()
+        {
+            if (koren == null)
+            {
+                return 0;
+            }
+            return VyskaRekur(koren) - 1;
+        }
+
+        private int VyskaRekur(Node koren)
+        {
+            if (koren == null)
+            {
+                return 0;
+            }
+            int levy = VyskaRekur(koren.levy) + 1;
+            int pravy = VyskaRekur(koren.pravy) + 1;
+            return levy > pravy ? levy : pravy;
+        }
+
+        public void Add(T vstup)
+        {
+            if (koren == null)
+            {
+                koren = new Node(vstup);
+                return;
+            }
+            AddRekur(koren, vstup);
+        }
+
+        private void AddRekur(Node koren, T vstup)
+        {
+            if (vstup.CompareTo(koren.hodn) <= 0)
+            {
+                if (koren.levy != null)
+                {
+                    AddRekur(koren.levy, vstup);
+                }
+                else
+                {
+                    koren.levy = new Node(vstup);
+                }
+            }
+            else
+            {
+                if (koren.pravy != null)
+                {
+                    AddRekur(koren.pravy, vstup);
+                }
+                else
+                {
+                    koren.pravy = new Node(vstup);
+                }
+            }
+        }
+        public void VypisObsah()
+        {
+            if (koren != null)
+            {
+                VypisObsahRekur2(koren);
+            }
+        }
+
+        private void VypisObsahRekur2(Node koren)
+        {
+            Console.WriteLine(koren.hodn);
+            if (koren.levy != null)
+            {
+                VypisObsahRekur(koren.levy);
+            }
+            if (koren.pravy != null)
+            {
+                VypisObsahRekur(koren.pravy);
+            }
+        }
+
+        private void VypisObsahRekur(Node koren)
+        {
+            if (koren.levy != null)
+            {
+                VypisObsahRekur(koren.levy);
+            }
+            Console.WriteLine(koren.hodn);
+            if (koren.pravy != null)
+            {
+                VypisObsahRekur(koren.pravy);
+            }
+        }
+
+        public void SmazatUzel(T vstup)
+        {
+            Node node = new Node(vstup);
+            SmazatUzelRekur(koren, node);
+        }
+
+        private Node SmazatUzelRekur(Node koren, Node node)
+        {
+            if (koren == null)
+            {
+                return koren;
+            }
+
+            if (node.hodn.CompareTo(koren.hodn) < 0)
+            {
+                koren.levy = SmazatUzelRekur(koren.levy, node);
+            }
+            else if (node.hodn.CompareTo(koren.hodn) > 0)
+            {
+                koren.pravy = SmazatUzelRekur(koren.pravy, node);
+            }
+
+            else
+            {
+                if (koren.levy == null)
+                {
+                    return koren.pravy;
+                }
+                else if (koren.pravy == null)
+                {
+                    return koren.levy;
+                }
+                koren.hodn = NejmensiHodn(koren.pravy);
+
+                Node temp = new Node(koren.hodn);
+                koren.pravy = SmazatUzelRekur(koren.pravy, temp);
+            }
+            return koren;
+        }
+
+        private T NejmensiHodn(Node koren)
+        {
+            T min = koren.hodn;
+            while (koren.levy != null)
+            {
+                min = koren.levy.hodn;
+                koren = koren.levy;
+            }
+            return min;
+        }
+
+        //Vykresleni
+        private void Print(Node root, int topMargin = 2, int leftMargin = 2)
+        {
+            if (root == null) return;
+            int rootTop = Console.CursorTop + topMargin;
+            var last = new List<NodeInfo>();
+            var next = root;
+            for (int level = 0; next != null; level++)
+            {
+                var item = new NodeInfo { Node = next, Text = next.hodn.ToString() };
+                if (level < last.Count)
+                {
+                    item.StartPos = last[level].EndPos + 1;
+                    last[level] = item;
+                }
+                else
+                {
+                    item.StartPos = leftMargin;
+                    last.Add(item);
+                }
+                if (level > 0)
+                {
+                    item.Parent = last[level - 1];
+                    if (next == item.Parent.Node.levy)
+                    {
+                        item.Parent.Left = item;
+                        item.EndPos = Math.Max(item.EndPos, item.Parent.StartPos);
+                    }
+                    else
+                    {
+                        item.Parent.Right = item;
+                        item.StartPos = Math.Max(item.StartPos, item.Parent.EndPos);
+                    }
+                }
+                next = next.levy ?? next.pravy;
+                for (; next == null; item = item.Parent)
+                {
+                    Print(item, rootTop + 2 * level);
+                    if (--level < 0) break;
+                    if (item == item.Parent.Left)
+                    {
+                        item.Parent.StartPos = item.EndPos;
+                        next = item.Parent.Node.pravy;
+                    }
+                    else
+                    {
+                        if (item.Parent.Left == null)
+                            item.Parent.EndPos = item.StartPos;
+                        else
+                            item.Parent.StartPos += (item.StartPos - item.Parent.EndPos) / 2;
+                    }
+                }
+            }
+            Console.SetCursorPosition(0, rootTop + 2 * last.Count - 1);
+        }
+
+        private static void Print(NodeInfo item, int top)
+        {
+            SwapColors();
+            Print(item.Text, top, item.StartPos);
+            SwapColors();
+            if (item.Left != null)
+                PrintLink(top + 1, "┌", "┘", item.Left.StartPos + item.Left.Size / 2, item.StartPos);
+            if (item.Right != null)
+                PrintLink(top + 1, "└", "┐", item.EndPos - 1, item.Right.StartPos + item.Right.Size / 2);
+        }
+
+        private static void PrintLink(int top, string start, string end, int startPos, int endPos)
+        {
+            Print(start, top, startPos);
+            Print("─", top, startPos + 1, endPos);
+            Print(end, top, endPos);
+        }
+
+        private static void Print(string s, int top, int left, int right = -1)
+        {
+            Console.SetCursorPosition(left, top);
+            if (right < 0) right = left + s.Length;
+            while (Console.CursorLeft < right) Console.Write(s);
+        }
+
+        private static void SwapColors()
+        {
+            var color = Console.ForegroundColor;
+            Console.ForegroundColor = Console.BackgroundColor;
+            Console.BackgroundColor = color;
+        }
+    }
+}
---
+++ PRG/DatoveStruktury/DatoveStruktury/Program.cs
@@ -0,0 +1,196 @@
+﻿using System;
+
+namespace DatoveStruktury
+{
+    class SpojovySeznam<T> where T : IComparable
+    {
+        Uzel zacatek;
+        class Uzel
+        {
+            public T data;
+            public Uzel dalsi;
+        }
+
+        public void VlozNaZacatek(T vstup)
+        {
+            Uzel novy = new Uzel();
+            novy.data = vstup;
+            if (zacatek == null)
+            {
+                zacatek = novy;
+            }
+            else
+            {
+                novy.dalsi = zacatek;
+                zacatek = novy;
+            }
+        }
+
+        public void VlozNaKonec(T vstup)
+        {
+            Uzel novy = new Uzel();
+            novy.data = vstup;
+
+            Uzel docasny = zacatek;
+            
+            if (zacatek == null)
+            {
+                zacatek = novy;
+                return;
+            }
+            while (docasny.dalsi != null)
+            {
+                docasny = docasny.dalsi;
+            }
+
+            docasny.dalsi = novy;
+            novy.dalsi = null;
+        }
+
+        public void VypisPrvky()
+        {
+            Uzel novy = new Uzel();
+            novy = zacatek;
+            if (zacatek != null)
+            {
+                if (zacatek.dalsi == null)
+                {
+                    Console.WriteLine(zacatek.data);
+                }
+                else
+                {
+                    while (novy.dalsi != null)
+                    {
+                        Console.WriteLine(novy.data);
+                        novy = novy.dalsi;
+                    }
+                    Console.WriteLine(novy.data);
+                }
+            }
+        }
+
+        public void OdebraniPodleHodnoty(T hodnota)
+        {
+            Uzel uzel = zacatek;
+            Uzel predchozi = new Uzel();
+            while (hodnota.CompareTo(uzel.data) != 0)
+            {
+                if (uzel.dalsi != null)
+                {
+                    predchozi = uzel;
+                    uzel = uzel.dalsi;
+                }
+                else
+                {
+                    throw new Exception();
+                }
+            }
+            predchozi.dalsi = uzel.dalsi;
+        }
+
+        public void OdebraniPodleIndexu(int index)
+        {
+            if (index == 0)
+            {
+                zacatek = zacatek.dalsi;
+            }
+            else
+            {
+                Uzel pointer = zacatek;
+                int counter = 1;
+
+                while (pointer.dalsi != null)
+                {
+                    if (counter == index)
+                    {
+                        pointer.dalsi = pointer.dalsi.dalsi;
+                        break;
+                    }
+
+                    counter += 1;
+                    pointer = pointer.dalsi;
+                }
+            }
+        }
+
+        public void VlozNaIndex(int index, T vstup)
+        {
+            if (index == 0)
+            {
+                VlozNaZacatek(vstup);
+            }
+            else
+            {
+                Uzel novy = new Uzel();
+                novy.data = vstup;
+
+                Uzel pointer = zacatek;
+                int counter = 1;
+
+                while (pointer.dalsi != null)
+                {
+                    if (counter == index)
+                    {
+                        novy.dalsi = pointer.dalsi;
+                        pointer.dalsi = novy;
+                        break;
+                    }
+
+                    counter += 1;
+                    pointer = pointer.dalsi;
+                }
+            }
+        }
+
+        public void VlozSerazene(T vstup)
+        {
+            Uzel novy = new Uzel();
+            novy.data = vstup;
+            if (zacatek == null || vstup.CompareTo(zacatek.data) < 0)
+            {
+                novy.dalsi = zacatek;
+                zacatek = novy;
+            }
+            else
+            {
+                Uzel temp = zacatek;
+                while (temp.dalsi != null && vstup.CompareTo(temp.dalsi.data) > 0)
+                {
+                    temp = temp.dalsi;
+                }
+                novy.dalsi = temp.dalsi;
+                temp.dalsi = novy;
+            }
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SpojovySeznam<int> seznam = new SpojovySeznam<int>();
+            //seznam.VlozNaZacatek(1);
+            //seznam.VlozNaZacatek(2);
+            //seznam.VlozNaZacatek(3);
+
+            //seznam.VlozNaKonec(1);
+            //seznam.VlozNaKonec(2);
+            //seznam.VlozNaKonec(3);
+
+            //seznam.VlozNaIndex(2, 4);
+            //seznam.VlozNaIndex(4, 5);
+            //seznam.VlozNaIndex(6, 6);
+
+            //seznam.OdebraniPodleIndexu(2);
+            //seznam.OdebraniPodleHodnoty(4);
+
+            seznam.VlozSerazene(5);
+            seznam.VlozSerazene(9);
+            seznam.VlozSerazene(1);
+            seznam.VlozSerazene(3);
+            seznam.VlozSerazene(12);
+
+            seznam.VypisPrvky();
+        }
+    }
+}
